4532b94484b0ef2e13e07d07f0cc15876a6ee3af
Farida <faridah.absi@gmail.com>
2018-10-27 18:47:07 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom';
 import ToggleButton from './components/ToggleButton';
  
 ReactDOM.render(
-  <ToggleButton />,
+  <Address 
+    street="Santa Monica Blvd."
+    city="Santa Monica"
+  />,
   document.getElementById('root')
 );
